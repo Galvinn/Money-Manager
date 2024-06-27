d52efcb8f7045ebbbf962ec0a75bcc9ab213e01b
Galvinn <liuhaiwen26@hotmail.com>
2024-06-27 21:19:29 +0200

    added Bottombar Icons assets

---
+++ MoneyApp/constants/icons.js
@@ -0,0 +1,43 @@
+import overview from "../assets/icons/overview.png"
+import budget from "../assets/icons/budget.png"
+import saving from "../assets/icons/saving.png"
+import dashboard from "../assets/icons/dashboard.png"
+import add from "../assets/icons/add.png"
+
+
+
+import bookmark from "../assets/icons/bookmark.png";
+import home from "../assets/icons/home.png";
+import plus from "../assets/icons/plus.png";
+import profile from "../assets/icons/profile.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import menu from "../assets/icons/menu.png";
+import search from "../assets/icons/search.png";
+import upload from "../assets/icons/upload.png";
+import rightArrow from "../assets/icons/right-arrow.png";
+import logout from "../assets/icons/logout.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import play from "../assets/icons/play.png";
+
+export default {
+  overview,
+  budget,
+  saving,
+  dashboard,
+  add,
+
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
